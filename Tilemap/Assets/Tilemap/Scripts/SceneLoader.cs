78b5b83cf33eee64855d8f27bd2b002a455c9848
AndreRab <90551047+AndreRab@users.noreply.github.com>
2023-02-02 14:29:02 +0100

    Settings Design

--- Tilemap/Assets/Tilemap/Scripts/SceneLoader.cs
+++ Tilemap/Assets/Tilemap/Scripts/SceneLoader.cs
@@ -6,7 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public GameObject starterPanel;
+    public GameObject settingsPanel;
+    
     public void StartGame(){
         SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OpenSettings(){
+        starterPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings(){
+        starterPanel.SetActive(true);
+        settingsPanel.SetActive(false);
     }
 }
